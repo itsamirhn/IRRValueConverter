4aebd25d71bd7306f8029262412baf7144acc164
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-05-24 00:22:27 +0430

    Initial Pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# 🇮🇷 IRR Value Converter
+In Iran, value of everything changes every day.
+This tool convert value of money on specific date to value of money on today.
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="text-center">
+    <v-row>
+      <v-col>
+        <h1>تبدیل ارزش ریال 🇮🇷</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p>با این سایت میتونین قیمت  هرچیزی در هر چند وقت گذشته رو به ارزش الانش تبدیل کنین :))</p>
+      </v-col>
+    </v-row>
+    <v-form>
+      <v-row>
+        <v-col>
+          <v-text-field v-model="amount" label="قیمت" aria-label="Amount" required/>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-date-picker
+            v-model="date"
+            locale="fa-IR"
+            first-day-of-week="6"
+            aria-label="Date"
+          ></v-date-picker>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      amount: '',
+      date: ''
+    }
+  }
+}
+</script>
