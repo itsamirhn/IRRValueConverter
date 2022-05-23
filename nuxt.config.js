fb5d732a72a14f13a1cf2c16591da7abfa4ec2ed
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-05-24 00:28:59 +0430

    Prepare gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,12 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  target: 'static',
+
+  router: {
+    base: 'IRRValueConverter'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - IRRValueConverter',
